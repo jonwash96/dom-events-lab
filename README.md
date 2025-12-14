3f152268e65d55f9aed6cac35e8fff025eebfff7
JON Washington <jonwashPro@gmail.com>
2025-12-13 21:29:27 -0600

    Init commit of 2Operand simple version

--- README.md
+++ README.md
@@ -1,47 +1,20 @@
-# Basic Calculator | DOM Events Lab
+# Basic Calculator | Simple 2-Operand version | DOM Events Lab
 
 ---
 
+After receiving feedback on my first submittion about my use of eval(), and my divergence from the lesson, I decided to complete the simple 2-Operand version, so as to not loose sight of the objective— but with the added challenge of using the most concise possible expressions (whithin reason), while still maintaining readable code, and a solid UX.
+> This 2-Operand version is over 100 lines of code shorter than my first version.
+
 ### Features
 
-- Full Backspace (Backspace Key only)
-- Calculation History
-- 8 Digit max numbers
-- 50 Character max calculation
-- Keyboard Support
+- Add 2 numbers together
+- Show operation mode on left-hand side
 - Minimal Skeumorphic design
+- Calculate result on any seccond operator keypress (neither a bug nor real feature)
+
+### Technical Features
+- Code Uses event bubling instead of dedicated listeners
+- Uses recusion
+- Uses the nullish coalescing operator
 
 > Note: Default font may not be availible on all devices. Please see dafont.com > DS Digital to download on your system. Fallback fonts are set, but do not satisfy the visual intended aesthetic.
-
----
-
-## Version 0.4
-
-### Bug Fixes
-- Fixed calculation/order of operations issues
-- Doesn't Do math properly
-- Continuing the operation after Backspace doesn't work
-- Switching between double negative and change to positive doesn't work properly.
-
----
-
-## Version 0.1
-
-### Features
-
-- Full Backspace (Backspace Key only)
-- Calculation History
-- 8 Digit max numbers
-- 50 Character max calculation
-- Keyboard Support
-- Minimal Skeumorphic design
-
-### Bugs
-- [x] Doesn't Do math properly
-- [x] Continuing the operation after Backspace doesn't work
-- [] Does not support decimal numbers
-- [] No button for backspace
-- [] No history
-- [] No memory
-- [x] Switching between double negative and change to positive doesn't work properly. (call .back()?)
-- [x] Calculation/Order of operations: The result is calculated each time an operator is pressed, thus, each new number operates on the previous result, thus the only order of operations, is the accumulated value, operated upon by the recently types operator and value. Basically, the calculator performs only the most basic of mathematatic operations.
--- basic-version/dom-events-lab/app.js
+++ basic-version/dom-events-lab/app.js
@@ -12,29 +12,30 @@ let operation = [[]];
 
 // FUNCTIONS
 function handleButtonPress(e) {
-    let value = e.target?.textContent || e; // Distinguish between button press and '=' recursion
+    let value = e.target?.textContent || e; // distinguish between button press and '=' recursion
     if (!/^[\dC\+-\/\*=]$/.test(value)) return; // Ignore non-button events
     switch (value) {
-        case 'C': {operation[1] ? operation.splice(-1,1) : reset('f')}; break; // If 2 nums: remove last, otherwise reset
-        case '=': {handleButtonPress(operator)}; break; // Recurse this function, passing in the current operator, registering a seccond operator press
+        case 'C': {operation[1] ? operation.splice(-1,1) : reset('f')}; break; // if 2 nums: remove last, otherwise reset
+        case '=': {handleButtonPress(operator)}; break; // recurse this function, passing in the current operator, registering a seccond operator press
         // For operators: Push value the first time (it was an operator button press); Evaluate the seccond time (called by '=' press)
         case '+': {operation[1] ? render(num(operation[0]) + num(operation[1])) : operator = value}; break;
         case '-': {operation[1] ? render(num(operation[0]) - num(operation[1])) : operator = value}; break;
         case '*': {operation[1] ? render(num(operation[0]) * num(operation[1])) : operator = value}; break;
         case '/': {operation[1] ? render(num(operation[0]) / num(operation[1])) : operator = value}; break;
         // For numbers: push to first or seccond array
-        default: {operation[1] ? operation[1].push(value) : operation[0].push(value); render('number')};
-    };
+        default: {operation[1] ? operation[1].push(value) : operation[0].push(value); render('number'); console.log(operation)};
+        };
         // Clear state after render result. Render only the current operation the first time 
         if (/^[\+-\/\*]$/.test(value)) {
+            console.log("operator:",value);
             if (operation[1]) {reset();
-            } else {render('operator'); operation.push([])};
+            } else {render('operator'); operation.push([]);console.log(operation)};
         };
 };
 
+
 // FUNCTIONS
-function num(val) {return Array.isArray(val) ? Number(val.join('')) : Number(val)};
-function reset(option) {operator = null; operation = [[]]; /^f$/.test(option) && render(0)}; // The 'f' option re-render's the display with 0
+function num(val) {console.log("Value:",val); return Array.isArray(val) ? Number(val.join('')) : Number(val)};
 
 function render(option) {
     let value;
@@ -46,5 +47,7 @@ function render(option) {
     display.textContent = value;
     operator ?? (displayOp.textContent = '');
 }
+
+function reset(option) {operator = null; operation = [[]]; /^f$/.test(option) && render(0)}; // The 'f' option re-render's the display with 0
 
 render(0);
